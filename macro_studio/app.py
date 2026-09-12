2ebb9e17e4d16f919c5edcb1be0b859542aad37e
nonojin99 <nonojin5094@gmail.com>
2026-09-12 14:20:14 +0900

    fix: restore full MacroStudioApp shell

--- macro_studio/app.py
+++ macro_studio/app.py
@@ -17,3 +17,11 @@ from .ui_events_edit import EventsEditUIMixin
 from .app_hotkeys import HotkeysUIMixin
 from .ui_share import ShareUIMixin
 from .ui_slots import SpotsUIMixin
+from .ui_slots_ops import SlotsOpsUIMixin
+
+APP_TITLE = "매크로 스튜디오 (Macro Studio)"
+
+
+class MacroStudioApp(SlotsUIMixin, SlotsOpsUIMixin, ShareUIMixin, ClipboardUIMixin, EventsUIMixin, EventsEditUIMixin, HotkeysUIMixin, ctk.CTk):
+    def __init__(self) -> None:
+        super().__init__()
